9bcca12425d5997006a80a14661fad0fe23bf422
root <root@DESKTOP-MUBFNSQ.localdomain>
2022-06-13 11:40:01 -0700

    Update

--- app/concat.js
+++ app/concat.js
@@ -3,5 +3,4 @@ const converted = require('./converted')
 const lodash = require('lodash')
 
 const concatedArr = lodash.concat(converted.jsonArr.data, appArr.appArr )
-
 module.exports.concatedArray = concatedArr;
